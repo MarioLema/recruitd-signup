60e0b1681399fe3b4c40b0a767a0333e6b97a05d
Mario Lema <lema_mario@yahoo.com>
2019-04-26 17:42:25 +0100

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import bottomRightShape from "./images/bottom_right_shape.png";
+import topLeftShape from "./images/top_left_shape.png";
+import logo from "./images/recruitd_MainLogo_Green.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="holder logo-holder">
+        <img className="shape shape--top" src={topLeftShape} alt="" />
+        <img className="shape shape--bottom" src={bottomRightShape} alt="" />
+        <img src={logo} alt="" />
+      </div>
+      <div className="holder form-holder">
+        <div>
+          <div className="signup-link">
+            Already have an account? <a href="/signup">Sign in here</a>
+          </div>
+          <form>
+            <div>
+              <label className="box-label" htmlFor="cheese">
+                Full Name
+              </label>
+              <input
+                className="input-box"
+                type="text"
+                placeholder="Full Name"
+              />
+            </div>
+            <div>
+              <label className="box-label" htmlFor="cheese">
+                Email Address
+              </label>
+              <input className="input-box" type="email" placeholder="Email" />
+            </div>
+            <div>
+              <label className="box-label" htmlFor="cheese">
+                Username
+              </label>
+              <input className="input-box" type="text" placeholder="Username" />
+            </div>
+            <div>
+              <label className="box-label" htmlFor="cheese">
+                Password
+              </label>
+              <input
+                className="input-box"
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div>
+              <label htmlFor="cheese" className="checkbox-label">
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  name="t&c"
+                  value="t&c"
+                />
+                <span className="checkmark" />
+                Creating an account means you have read and accept our{" "}
+                <a href="/signup">Privacy Policy</a> and{" "}
+                <a href="/signup">Terms of Use</a>.
+              </label>
+            </div>
+            <div>
+              <button type="submit">Create Account</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
