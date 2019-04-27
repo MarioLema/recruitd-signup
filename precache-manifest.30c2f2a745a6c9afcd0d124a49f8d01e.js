edae6306510a3d22c0e093609727f37f744484c4
Mario Lema <lema_mario@yahoo.com>
2019-04-27 14:51:32 +0100

    Updates

---
+++ precache-manifest.30c2f2a745a6c9afcd0d124a49f8d01e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab79e0f5e3dc4bf81c936a6900d4cd05",
+    "url": "/recruitd-signup/index.html"
+  },
+  {
+    "revision": "114d8060e9ba5d730ccb",
+    "url": "/recruitd-signup/static/css/main.83e5e669.chunk.css"
+  },
+  {
+    "revision": "69c3d0ce72bd53eab8c7",
+    "url": "/recruitd-signup/static/js/2.7b7097b1.chunk.js"
+  },
+  {
+    "revision": "114d8060e9ba5d730ccb",
+    "url": "/recruitd-signup/static/js/main.593b55fe.chunk.js"
+  },
+  {
+    "revision": "dc1a8115c565af280822",
+    "url": "/recruitd-signup/static/js/runtime~main.00c2ae47.js"
+  },
+  {
+    "revision": "1a7f90ff1f1ec75ed4e588736c6a81b0",
+    "url": "/recruitd-signup/static/media/Inter-Regular.1a7f90ff.woff"
+  },
+  {
+    "revision": "4b2e4f39d8507d7f09c8944623823de1",
+    "url": "/recruitd-signup/static/media/Inter-Regular.4b2e4f39.otf"
+  },
+  {
+    "revision": "6e1bdd3689fad8d63f6d17c89fdeb3b1",
+    "url": "/recruitd-signup/static/media/Inter-Regular.6e1bdd36.ttf"
+  },
+  {
+    "revision": "d55e957612a3b2a373925d920f4af6b1",
+    "url": "/recruitd-signup/static/media/Inter-Regular.d55e9576.woff2"
+  },
+  {
+    "revision": "0c93dbf4f8979eb60beec74901730510",
+    "url": "/recruitd-signup/static/media/shadow1.0c93dbf4.svg"
+  },
+  {
+    "revision": "ba7bc8c69177fe271c6746ea64a49917",
+    "url": "/recruitd-signup/static/media/shadow2.ba7bc8c6.svg"
+  },
+  {
+    "revision": "928ba0557c4ae6e29446758cabb5cb0a",
+    "url": "/recruitd-signup/static/media/shape1.928ba055.svg"
+  },
+  {
+    "revision": "5e2bebb36fa975d7d4a890ec263b1721",
+    "url": "/recruitd-signup/static/media/shape2.5e2bebb3.svg"
+  },
+  {
+    "revision": "4c952e26758504ed52de9789e830f976",
+    "url": "/recruitd-signup/static/media/shape3.4c952e26.svg"
+  },
+  {
+    "revision": "e1c5700d9f7168b65e72be5a379e5f1c",
+    "url": "/recruitd-signup/static/media/shape4.e1c5700d.svg"
+  }
+]);
